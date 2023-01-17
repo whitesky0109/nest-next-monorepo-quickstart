f840c7a92465d03da360600ed41a734caf258205
조건우 <gunwoo.cho@hanwha.com>
2023-01-17 16:35:45 +0900

    [main][my-server] integration my-react

--- packages/my-server/pages/views/about.tsx
+++ packages/my-server/pages/views/about.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { NextPage } from 'next';
 
+import { Button } from 'my-react';
+
 const Page: NextPage = () => {
-  return <div>About Page!</div>;
+  return <div>
+    About Page!
+    <Button label="hello world" />
+  </div>;
 };
 
 export default Page;
