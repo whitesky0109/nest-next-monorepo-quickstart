f757ad29addb772708f4ef50a1b99ffa6f1dd94e
조건우 <gunwoo.cho@hanwha.com>
2023-01-17 20:05:51 +0900

    [main][my-server] appliecd passport-local + jwt

--- packages/my-server/src/auth/auth.controller.ts
+++ packages/my-server/src/auth/auth.controller.ts
@@ -1,15 +1,17 @@
 import {
-    Controller, Post, Request, UseGuards, 
-  } from '@nestjs/common';
-  
-  import { LocalAuthGuard } from './strategies/local/guard';
-  
-  @Controller('auth')
-  export class AuthController {
-    @UseGuards(LocalAuthGuard)
-    @Post('/login/local')
-    async login(@Request() req) {
-      return req.user;
-    }
+  Controller, Post, Request, UseGuards, 
+} from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './strategies/local/guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login/local')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
-  
+}
--- packages/my-server/src/auth/auth.module.ts
+++ packages/my-server/src/auth/auth.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from '../users/users.module';
 
-import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategies/local/strategy';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { LocalStrategy } from './strategies/local/strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
